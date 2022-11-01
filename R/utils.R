b2926e00ee53fcc3aebe908e3db94f177ad0e25c
Ethan Bass <ethanbass@gmail.com>
2022-11-01 16:21:26 -0400

    add plot option to find_peak

--- R/utils.R
+++ R/utils.R
@@ -19,12 +19,13 @@ tidy_eispectrum <- function(x){
 #' @param mz Quant.mass
 #' @param rt.tol Tolerance for matching retention time
 #' @param mz.tol Tolerance for matching Quant.mass
+#' @param plot_it Logical. Whether to plot the spectra.
 #' @importFrom stringr str_split_fixed
 #' @return Returns EI spectrum as a \code{data.frame}.
 #' @author Ethan Bass
 #' @export
 
-find_peak <- function(x, rt, mz, rt.tol = .01, mz.tol =.05){
+find_peak <- function(x, rt, mz, rt.tol = .01, mz.tol =.05, plot_it = TRUE){
   pks <- seq_len(ncol(x$tab))
   if (!missing(rt)){
     pks1 <- which(abs(rt - as.numeric(x$peak_meta[,"Average.Rt.min."])) < rt.tol)
@@ -32,5 +33,10 @@ find_peak <- function(x, rt, mz, rt.tol = .01, mz.tol =.05){
   if (!missing(mz)){
     pks2 <- which(abs(mz - as.numeric(x$peak_meta[,"Quant.mass"])) < mz.tol)
   } else{pks2 <- pks}
-  intersect(pks1, pks2)
+  pks <- intersect(pks1, pks2)
+  if (plot_it)
+    for (pk in pks){
+      plot_spectrum(x, pk, type = "base")
+    }
+  pks
 }
