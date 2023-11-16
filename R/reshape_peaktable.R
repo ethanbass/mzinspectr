87bbf633ec5059d2a0d6b8728295a6dbaf6aba05
Ethan Bass <ethanbass@gmail.com>
2023-11-16 15:40:16 -0500

    feat: add fixed_levels argument to reshape_peaktable

--- R/reshape_peaktable.R
+++ R/reshape_peaktable.R
@@ -10,6 +10,7 @@
 #' the tidy output.
 #' @param treatments This argument is deprecated as of version 0.3.2. It is
 #' synonymous with the new metadata argument which should be used instead.
+#' @param fixed_levels Fix factor levels of features in the order provided.
 #' @importFrom dplyr select mutate any_of
 #' @importFrom tidyr pivot_longer
 #' @return If \code{export} is \code{TRUE}, returns spectrum as \code{data.frame}.
@@ -17,29 +18,34 @@
 #' @author Ethan Bass
 #' @export
 
-ms_reshape_peaktable <- function(x, peaks, metadata, treatments = NULL){
+ms_reshape_peaktable <- function(x, peaks, metadata, treatments = NULL,
+                                 fixed_levels = TRUE){
   if (!is.null(treatments)){
     .Deprecated("metadata", package="msdialreadr", old = "treatments",
                 msg="The `treatments` argument is deprecated as of msdialreadr v0.3.2.
                 \t Please use the `metadata` argument instead.")
     metadata <- treatments
   }
+  df <- as.data.frame(x$tab)
   if (!missing(peaks)){
     if (is.numeric(peaks)){
       peaks <- colnames(df)[peaks]
     }
-    x$tab <- x$tab[,which(colnames(x$tab) %in% peaks), drop = FALSE]
+    df <- df[,match(peaks,colnames(df)), drop = FALSE]
     if (!is.null(names(peaks))){
-      colnames(x$tab) <- names(peaks)[match(colnames(x$tab), peaks)]
-      peaks <- colnames(x$tab)
+      colnames(df) <- names(peaks)
+      peaks <- colnames(df)
     }
   }
-  df <- as.data.frame(x$tab)
-  df <- cbind(df, select(x$sample_meta, any_of(metadata)))
-  df <- mutate(df, sample = row.names(x))
-  df <- pivot_longer(df, cols = peaks, names_to = "peak")
+  df <- cbind(df, dplyr::select(x$sample_meta, dplyr::any_of(metadata)))
+  df <- dplyr::mutate(df, sample = row.names(x))
+  df <- tidyr::pivot_longer(df, cols = peaks, names_to = "peak")
+  if (fixed_levels){
+    df$peak <- factor(df$peak, levels = peaks)
+  }
   df
 }
+
 
 #' Reshape peak table
 #'
